b32aaaafeee52b623750464e0e4ae97da0e936f4
Benlego1817 <97744790+Benlego1817@users.noreply.github.com>
2025-03-11 10:22:47 +0100

    Add Diagonal Movement

--- object.py
+++ object.py
@@ -35,3 +35,21 @@ class SlidingObject(Object):
 
     def collide(self, other = None):
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+class GravityObject(Object):
+    def __init__(self, x: int=0, y: int=0, width: int=100, height:int=100, color=(255, 255, 255),screen=pygame.display.get_surface(), speedx: int = 1, speedy: int = 1):
+        super().__init__(x, y, width, height, color, screen)
+        self.speedx = speedx
+        self.speedy = speedy
+
+    def move(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > self.screen.get_width() or self.rect.left < 0:
+            self.speedx *= -1
+        if self.rect.bottom > self.screen.get_height() or self.rect.top < 0:
+            self.speedy *= -1
+
+
+
+
