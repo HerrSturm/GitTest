5c25e9689a666e336708421d7d68433e6eadac16
RealMoondancer <67159827+RealMoondancer@users.noreply.github.com>
2025-03-18 09:53:32 +0000

    Apply automatic changes

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pygame
 import sys
 from object import Object, SlidingObject, GravityObject, ShrinkingObject
 
+
 def main():
     pygame.init()
-    
+
     # Fenster-Einstellungen
     WIDTH, HEIGHT = 1200, 800
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,21 +18,18 @@ def main():
     gameObjects.append(SlidingObject(600, 200, 100, 100, (0, 255, 0), screen, -2))
     gameObjects.append(GravityObject(600, 200, 100, 100, (0, 255, 0), screen, 10, 0, 1))
     gameObjects.append(ShrinkingObject(800, 280, 100, 100, screen, -1))
-    
-
 
     clock = pygame.time.Clock()
     # Hauptschleife
     running = True
     while running:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         # Hintergrund färben
         screen.fill((0, 0, 0))  # Schwarz
-        
+
         # Objekte updaten (bewegen, zeichnen, etc.)
         for obj in gameObjects:
             obj.update()
@@ -41,15 +39,14 @@ def main():
             for other in gameObjects:
                 if obj != other and obj.rect.colliderect(other.rect):
                     obj.collide(other)
-        
-
 
         # Bildschirm aktualisieren
         pygame.display.flip()
         clock.tick(60)
-    
+
     pygame.quit()
     sys.exit()
+
 
 if __name__ == "__main__":
     main()
