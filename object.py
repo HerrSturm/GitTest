1b2d5dc01f570df6161be3202d8bc7b98b5b3737
Ditin2 <67159827+RealMoondancer@users.noreply.github.com>
2025-03-18 10:57:48 +0100

    Add github action to auto-reformat files (#8)
    * Create pylint.yml
    
    * Add ruff checks
    
    * Add autocommit
    
    * Apply automatic changes

--- object.py
+++ object.py
@@ -1,13 +1,22 @@
 from random import randint
 import pygame
 
+
 def sign(n):
     return -1 if n < 0 else 1
 
+
 # Elternklasse für Objekte
 class Object:
-    def __init__(self, x: int=0, y: int=0, width: int=100, height:int=100, color=(255, 255, 255),screen=pygame.display.get_surface()):
-
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        width: int = 100,
+        height: int = 100,
+        color=(255, 255, 255),
+        screen=pygame.display.get_surface(),
+    ):
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
         self.screen = screen
@@ -22,12 +31,21 @@ class Object:
         self.move()
         self.draw()
 
-    def collide(self, other = None):
+    def collide(self, other=None):
         pass
 
 
 class SlidingObject(Object):
-    def __init__(self, x: int=0, y: int=0, width: int=100, height:int=100, color=(255, 255, 255),screen=pygame.display.get_surface(), speed: int = 1):
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        width: int = 100,
+        height: int = 100,
+        color=(255, 255, 255),
+        screen=pygame.display.get_surface(),
+        speed: int = 1,
+    ):
         super().__init__(x, y, width, height, color, screen)
         self.speed = speed
 
@@ -36,11 +54,23 @@ class SlidingObject(Object):
         if self.rect.right > self.screen.get_width() or self.rect.left < 0:
             self.speed *= -1
 
-    def collide(self, other = None):
+    def collide(self, other=None):
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
+
 class GravityObject(Object):
-    def __init__(self, x: int=0, y: int=0, width: int=100, height:int=100, color=(255, 255, 255),screen=pygame.display.get_surface(), speedx: int = 1, speedy: int = 1, acc: int = 1):
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        width: int = 100,
+        height: int = 100,
+        color=(255, 255, 255),
+        screen=pygame.display.get_surface(),
+        speedx: int = 1,
+        speedy: int = 1,
+        acc: int = 1,
+    ):
         super().__init__(x, y, width, height, color, screen)
         self.speedx = speedx
         self.speedy = speedy
@@ -52,18 +82,22 @@ class GravityObject(Object):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.right > self.screen.get_width() or self.rect.left < 0:
-            self.rect.x = self.screen.get_width()/2 + sign(self.speedx)*(self.screen.get_width()/2)
+            self.rect.x = self.screen.get_width() / 2 + sign(self.speedx) * (
+                self.screen.get_width() / 2
+            )
             if self.speedx >= 0:
                 self.rect.x -= self.width
             self.speedx *= -1
         if self.rect.bottom > self.screen.get_height() or self.rect.top < 0:
-            self.rect.y = self.screen.get_height()/2 + sign(self.speedy)*(self.screen.get_height()/2)
+            self.rect.y = self.screen.get_height() / 2 + sign(self.speedy) * (
+                self.screen.get_height() / 2
+            )
             if self.speedy >= 0:
                 self.rect.y -= self.height
             self.speedy *= -1
         self.speedy += self.acc
 
-    def collide(self, other = None):
+    def collide(self, other=None):
         self.speedy -= self.acc
         self.rect.x -= self.speedx
         if not self.rect.colliderect(other.rect):
@@ -75,23 +109,39 @@ class GravityObject(Object):
         self.speedy += self.acc
 
 
-
-
 class ShrinkingObject(Object):
-    def __init__(self, x: int=0, y: int=0, width: int=100, height:int=100,screen=pygame.display.get_surface(), speed: int = 3):
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        width: int = 100,
+        height: int = 100,
+        screen=pygame.display.get_surface(),
+        speed: int = 3,
+    ):
         super().__init__(x, y, width, height, screen=screen)
         self.speed = speed
         self.collide_check = False
 
     def move(self):
         if not self.collide_check:
-            self.rect = pygame.Rect(self.rect.left - 1, self.rect.top - 1, self.rect.width + 2, self.rect.height + 2)
+            self.rect = pygame.Rect(
+                self.rect.left - 1,
+                self.rect.top - 1,
+                self.rect.width + 2,
+                self.rect.height + 2,
+            )
         self.collide_check = False
-        
+
         self.rect.x += self.speed
         if self.rect.right > self.screen.get_width() or self.rect.left < 0:
             self.speed *= -1
 
-    def collide(self, other = None):
+    def collide(self, other=None):
         self.collide_check = True
-        self.rect = pygame.Rect(self.rect.left + 5, self.rect.top + 5, self.rect.width - 10, self.rect.height - 10)
+        self.rect = pygame.Rect(
+            self.rect.left + 5,
+            self.rect.top + 5,
+            self.rect.width - 10,
+            self.rect.height - 10,
+        )
