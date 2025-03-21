dd71d92ad087ee2723b82f2186bfa89b4c49ea56
GYM-GCA-Schueler <GYM-GCA-Schueler@schulen.halle.de>
2025-03-21 12:23:51 +0100

    Add circle object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from object import Object, SlidingObject, GravityObject, ShrinkingObject
+from object import Object, SlidingObject, GravityObject, ShrinkingObject, Why
 
 
 def main():
@@ -14,10 +14,11 @@ def main():
     # Objekte erstellen und in Liste speichern
     gameObjects = []
     gameObjects.append(Object(100, 100, 100, 100, (255, 0, 0), screen))
-    gameObjects.append(SlidingObject(100, 200, 100, 100, (0, 255, 0), screen, 2))
-    gameObjects.append(SlidingObject(600, 200, 100, 100, (0, 255, 0), screen, -2))
+    #gameObjects.append(SlidingObject(100, 200, 100, 100, (0, 255, 0), screen, 2))
+    #gameObjects.append(SlidingObject(600, 200, 100, 100, (0, 255, 0), screen, -2))
     gameObjects.append(GravityObject(600, 200, 100, 100, (0, 255, 0), screen, 10, 0, 1))
-    gameObjects.append(ShrinkingObject(800, 280, 100, 100, screen, -1))
+    #gameObjects.append(ShrinkingObject(800, 280, 100, 100, screen, -1))
+    gameObjects.append(Why(WIDTH / 2, HEIGHT / 2, 100, 30, screen, 10))
 
     clock = pygame.time.Clock()
     # Hauptschleife
